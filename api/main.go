c01f07fa60fa7d2685d26f9b855e1de5666bbcb2
ricassiocosta <ricassio_costa@outlook.com>
2021-06-24 18:47:18 -0300

    configuring environment vars

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,8 +9,9 @@ import (
 )
 
 func main() {
+	config.LoadEnv()
 	r := router.Generate()
 
 	fmt.Println("Server is running...")
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.APIPort), r))
 }
